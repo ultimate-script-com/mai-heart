07a6de6df3fe55daf1436499b14088f355247782
ultimate-script-com <darkmatter8128@gmail.com>
2025-09-14 00:27:01 +0900

    upgrade

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,18 +1,17 @@
 import { type PageProps } from "$fresh/server.ts";
 export default function App({ Component }: PageProps) {
-    return (
-        <html>
-            <head>
-                <meta charset="utf-8" />
-                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                <title>fresh-site</title>
-                <link rel="stylesheet" href="/styles.css" />
-                
-                
-            </head>
-            <body>
-                <Component />
-            </body>
-        </html>
-    );
+  return (
+    <html>
+      <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>ultimate-script.com</title>
+        <meta name="description" content="関数アート（ハート型）" />
+        <link rel="stylesheet" href="/styles.css" />
+      </head>
+      <body>
+        <Component />
+      </body>
+    </html>
+  );
 }
